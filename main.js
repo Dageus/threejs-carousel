07d1d5d6f8ad98438bf478489eebbe246d0a30b0
Dageus <101069446+Dageus@users.noreply.github.com>
2024-05-19 14:17:16 +0100

    moved spotlight to mobius file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import MobiusStrip from './mobius';
+import Mobius from './mobius';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,20 +12,11 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
-const spotLight = new THREE.SpotLight('white', 0.7);
-spotLight.castShadow = true;
-spotLight.position.set(0, 1, 0);
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 0.1;
-spotLight.shadow.camera.far = 5;
-scene.add(spotLight);
+const mobius = new Mobius(scene, new THREE.BoxGeometry());
 
 scene.add(new THREE.AmbientLight('white', 0.3));
 
 
-const box = new THREE.BoxGeometry();
 const floor = new THREE.Mesh(box, new THREE.MeshStandardMaterial({
   color: new THREE.Color('steelblue')
 }));
@@ -37,7 +28,6 @@ scene.add(floor)
 
 camera.position.z = 5;
 
-const mobius = new MobiusStrip(scene, box);
 
 
 requestAnimationFrame(function render(t) {
@@ -59,4 +49,3 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
-// Path: mobius.js
--- mobius.js
+++ mobius.js
@@ -1,28 +1,46 @@
 import * as THREE from 'three';
 
-export default function MobiusStrip(scene, box) {
+export default function Mobius(scene, box) {
 
-  const count = 256;
-  const radius = 1.1;
-  this.strip = new THREE.Object3D();
-  scene.add(this.strip)
-  for (let i = 0; i < count; i++) {
-    const a = Math.PI / count * 2 * i;
-    const o = new THREE.Object3D();
-    o.position.set(Math.cos(a), Math.sin(a * 5) / 30, Math.sin(a))
-    o.position.multiplyScalar(radius);
-    o.lookAt(scene.position);
-    this.strip.add(o);
-    const mat = new THREE.MeshPhongMaterial({
-      color: new THREE.Color(`hsl(${a * 360 / Math.PI},55%,55%)`)
-    });
-    const mesh = new THREE.Mesh(box, mat);
-    mesh.scale.set(0.03, 0.3, 0.001)
-    mesh.castShadow = true;
-    //mesh.receiveShadow  = true;
-    mesh.rotation.x = a / 2;
-    o.add(mesh)
+  function MobiusSpotlight(scene) {
+    const spotLight = new THREE.SpotLight('white', 0.7);
+    spotLight.castShadow = true;
+    spotLight.position.set(0, 1, 0);
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+    spotLight.shadow.camera.near = 0.1;
+    spotLight.shadow.camera.far = 5;
+    scene.add(spotLight);
+  }
+
+  function MobiusStrip(scene, box) {
+
+    const count = 256;
+    const radius = 1.1;
+    this.strip = new THREE.Object3D();
+    scene.add(this.strip);
+    for (let i = 0; i < count; i++) {
+      const a = Math.PI / count * 2 * i;
+      const o = new THREE.Object3D();
+      o.position.set(Math.cos(a), Math.sin(a * 5) / 30, Math.sin(a))
+      o.position.multiplyScalar(radius);
+      o.lookAt(scene.position);
+      this.strip.add(o);
+      const mat = new THREE.MeshPhongMaterial({
+        color: new THREE.Color(`hsl(${a * 360 / Math.PI},55%,55%)`)
+      });
+      const mesh = new THREE.Mesh(box, mat);
+      mesh.scale.set(0.03, 0.3, 0.001)
+      mesh.castShadow = true;
+      //mesh.receiveShadow  = true;
+      mesh.rotation.x = a / 2;
+      o.add(mesh)
+    }
+
+
   }
 
 
+  this.MobiusStrip = new MobiusStrip(scene, box);
+  this.Spotlight = new MobiusSpotlight(scene);
 }
