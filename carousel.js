73f582287c196bbf0c3c2e02b76b32c54030aba8
Salvador Correia <salvador.correia@tecnico.ulisboa.pt>
2024-05-20 01:08:45 +0100

    Modelo carrosel

---
+++ carousel.js
@@ -0,0 +1,158 @@
+
+import * as THREE from "three";
+import { ThreeMFLoader } from "three/examples/jsm/Addons.js";
+import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
+//'use strict';
+
+// TODO usar ParametricGeometry porque era demasiado tarde para eu entender como isto funciona
+class Seats {
+    constructor(radius, height) {
+        this.seatGroup = new THREE.Group();
+
+        const numSeats = 8;
+        const angleIncrement = (2 * Math.PI) / numSeats;
+
+        for (let i = 0; i < numSeats; i++){
+            const angle = i * angleIncrement;
+
+            const x = radius * Math.cos(angle);
+            const y = height + 2.5;
+            const z = radius * Math.sin(angle);
+
+
+            const material = new THREE.MeshBasicMaterial({
+                color: Math.random() * 0xffffff,
+            });
+
+            const seat = new THREE.Mesh(new THREE.CylinderGeometry(2.5, 2.5, 5), material);
+            
+            seat.position.set(x, y, z); // Set position
+            seat.rotation.y = angle; // Rotate seat to match angle
+
+            this.seatGroup.add(seat);
+        }
+
+    }
+
+}
+
+
+class InnerRing {
+    constructor() {
+        const material = new THREE.MeshBasicMaterial({
+            color: THREE.Color.NAMES.red,
+        });
+        this.innerRing = new THREE.Group();
+        
+        this.upperRing = new THREE.Mesh(new THREE.RingGeometry(5, 15), material);
+        this.upperRing.rotateOnAxis(new THREE.Vector3(-1, 0, 0), Math.PI / 2);
+        this.upperRing.position.y = 30;
+        
+        this.lowerRing = new THREE.Mesh(new THREE.RingGeometry(5, 15), material);
+        this.lowerRing.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+        this.lowerRing.position.y = 20;
+
+        let path = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(0, 20, 0), // Start point (bottom)
+            new THREE.Vector3(0, 30, 0)   // End point (top)
+        ]);
+
+        this.outerWall = new THREE.Mesh(new THREE.TubeGeometry(path, 20, 15, 30, false), material);
+
+        this.seats = new Seats(10, 30).seatGroup;
+
+        this.innerRing.add(this.upperRing, this.lowerRing, this.outerWall, this.seats);
+    }
+
+}
+
+class MiddleRing {
+    constructor() {
+        const material = new THREE.MeshBasicMaterial({
+            color: THREE.Color.NAMES.green,
+        });
+        this.middleRing = new THREE.Group();
+        
+        this.upperRing = new THREE.Mesh(new THREE.RingGeometry(15, 25), material);
+        this.upperRing.rotateOnAxis(new THREE.Vector3(-1, 0, 0), Math.PI / 2);
+        this.upperRing.position.y = 20;
+        
+        this.lowerRing = new THREE.Mesh(new THREE.RingGeometry(15, 25), material);
+        this.lowerRing.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+        this.lowerRing.position.y = 10;
+
+        let path = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(0, 10, 0), // Start point (bottom)
+            new THREE.Vector3(0, 20, 0)   // End point (top)
+        ]);
+
+        this.outerWall = new THREE.Mesh(new THREE.TubeGeometry(path, 20, 25, 30, false), material);
+        
+        this.seats = new Seats(20, 20).seatGroup;
+
+        this.middleRing.add(this.upperRing, this.lowerRing, this.outerWall, this.seats);
+    }
+
+}
+
+class OuterRing {
+    constructor() {
+        const material = new THREE.MeshBasicMaterial({
+            color: THREE.Color.NAMES.blue,
+        });
+        this.outerRing = new THREE.Group();
+        
+        this.upperRing = new THREE.Mesh(new THREE.RingGeometry(25, 35), material);
+        this.upperRing.rotateOnAxis(new THREE.Vector3(-1, 0, 0), Math.PI / 2);
+        this.upperRing.position.y = 10;
+        
+        this.lowerRing = new THREE.Mesh(new THREE.RingGeometry(15, 25), material);
+        this.lowerRing.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+        this.lowerRing.position.y = 0;
+
+        let path = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(0, 0, 0), // Start point (bottom)
+            new THREE.Vector3(0, 10, 0)   // End point (top)
+        ]);
+
+        this.outerWall = new THREE.Mesh(new THREE.TubeGeometry(path, 20, 35, 30, false), material);
+        
+        this.seats = new Seats(30, 10).seatGroup;
+
+        this.outerRing.add(this.upperRing, this.lowerRing, this.outerWall, this.seats);
+    }
+
+}
+
+class Pillar {
+    constructor(material) {
+        let path = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(0, 0, 0), // Start point (bottom)
+            new THREE.Vector3(0, 50, 0)   // End point (top)
+        ]);
+
+        this.pillar = new THREE.Mesh(new THREE.TubeGeometry(path, 20, 5, 8, false), material);
+    }
+}
+
+
+class Carousel {
+  constructor() {
+    const material = new THREE.MeshBasicMaterial({
+      color: THREE.Color.NAMES.grey,
+    });
+
+    this.carouselGroup = new THREE.Group();
+
+    this.pillar = new Pillar(material).pillar;
+
+    this.outerRing = new OuterRing().outerRing;
+    this.middleRing = new MiddleRing().middleRing; 
+    this.innerRing = new InnerRing().innerRing;
+
+    this.carouselGroup.add(this.pillar, this.outerRing, this.middleRing, this.innerRing);
+  }
+
+}
+
+export default Carousel;
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
 import Mobius from './mobius';
+import Carousel from './carousel';
 import { InputManager } from './input-manager';
 import { Lights, Materials } from './objects';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 
 new InputManager();
 let materials = new Materials();
@@ -18,6 +20,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const mobius = new Mobius(scene, new THREE.BoxGeometry());
+const carousel = new Carousel();
+scene.add(carousel.carouselGroup);
 
 scene.add(new THREE.AmbientLight('white', 0.3));
 
@@ -26,14 +30,14 @@ const box = new THREE.BoxGeometry(10, 10, 10);
 const floor = new THREE.Mesh(box, new THREE.MeshStandardMaterial({
   color: new THREE.Color('steelblue')
 }));
-floor.scale.set(6, 0.01, 6);
+floor.scale.set(100, 0.01, 100);
 floor.position.y = -0.5;
 floor.castShadow = true;
 floor.receiveShadow = true;
 materials.addObject(floor);
 scene.add(floor)
 
-camera.position.set(5, 2.5, 5);
+camera.position.set(75, 90, 75);
 camera.lookAt(0, 0, 0);
 
 requestAnimationFrame(function render(t) {
